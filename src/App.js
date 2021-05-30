d40a4615ac2c8092488e8eaada7598e6d8df7f4d
arina <arina@mvalu.in>
2021-05-30 09:58:46 +0530

    initial commit for login with data that stored in redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from "./components/Login";
+import EmplyoeeList from "./components/EmplyoeeList";
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <BrowserRouter>
+              <Switch>
+                  <Route exact path='/' component={Login} />
+                  <Route exact path='/emplyoeeList' component={EmplyoeeList} />
+              </Switch>
+          </BrowserRouter>
       </header>
     </div>
   );
